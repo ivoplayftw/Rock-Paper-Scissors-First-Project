ab11f1aeaca66a7b3889ae61f119b7eca0c8209b
ivoplayftw <105995390+ivoplayftw@users.noreply.github.com>
2022-09-26 13:50:07 +0300

    Made the code faster, fixed scoring system.
    Replaced the While loops to For loops for faster code, and fixed the scoring system to keep the score and not reset it after each game.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,28 +1,28 @@
 import random
 import termcolor
+import sys
 
 rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
 player_points = 0
 computer_points = 0
-start_game = False
+started = 0
 
-while True:
+for start in range(started):
     print("To start the game type: start")
     start_input = input("").lower()
-    if start_input == "start":
-        start_game = True
+    if started == 0 and start_input == "start":
+        started += 1
         break
-    elif start_input == "stop":
+    elif started == 1 and start_input == "stop":
+        started -= 1
         break
     else:
         print("Invalid Input!")
 
 
-while start_game:
-    player_points = 0
-    computer_points = 0
+for _ in range(sys.maxsize):
     player_move = input("Choose [r]ock, [p]aper, [s]cissors: ")
     if player_move == "r":
         player_move = rock
@@ -34,7 +34,6 @@ while start_game:
         print("Invalid Input. Try again...")
         continue
 
-
     computer_random_number = random.randint(1, 3)
     computer_move = ''
     if computer_random_number == 1:
@@ -44,7 +43,6 @@ while start_game:
     elif computer_random_number == 3:
         computer_move = scissors
     print(f"The computer chose {computer_move}")
-
 
     if player_move == rock and computer_move == scissors or \
         player_move == paper and computer_move == rock or \
@@ -60,11 +58,13 @@ while start_game:
         print(termcolor.colored("You lose!", "red"))
         print(f"Player: {player_points} / Player_2: {computer_points}")
 
-
     print("Do you want to play again?")
     print("Type 'yes' to continue or 'no' to exit the game.")
     again = input('')
     if again.lower() == "yes":
         continue
     if again.lower() == "no":
-        start_game = False
+        break
+    else:
+        print("Invalid input!")
+        break
